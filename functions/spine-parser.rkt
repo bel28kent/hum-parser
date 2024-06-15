079020b70bd35c79d412c76a3647bad42a7c9e70
Bryan Bell <bjbsga@gmail.com>
2024-06-15 13:36:53 -0700

    requires, provides, and function signature for spine-parser

--- functions/spine-parser.rkt
+++ functions/spine-parser.rkt
@@ -1,1 +1,12 @@
 #lang racket
+
+(require "../data-definitions/data-definitions.rkt"
+         test-engine/racket-tests)
+
+(provide (all-defined-out))
+
+; TODO: spine-parser
+; HumdrumFile SpineArity -> (listof GlobalSpine)
+; produces a list of global spines from the HumdrumFile
+
+(define (spine-parser hfile spine-arity) (void))
