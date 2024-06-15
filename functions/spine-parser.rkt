471f1e541be2b44fa6910ea7e9736f843fec6ba4
Bryan Bell <bjbsga@gmail.com>
2024-06-15 16:06:03 -0700

    implements unwrap and byrecord->byspine functions

--- functions/spine-parser.rkt
+++ functions/spine-parser.rkt
@@ -1,6 +1,7 @@
 #lang racket
 
 (require "../data-definitions/data-definitions.rkt"
+         "../functions/abstract.rkt"
          test-engine/racket-tests)
 
 (provide (all-defined-out))
@@ -23,3 +24,51 @@
 ;    c. Once counter eq the first, put the rest of lot on to an accumulator for the next spine
 ;    d. Recurse with the rest of lon and rest of lolot
 ; 3. With accumulator of 2b, make-global-spine for each lot
+
+; unwrap
+; HumdrumFile -> (listof (listof Token))
+; produces the list of each record's list of tokens
+
+(define (unwrap hfile)
+  (local [(define records (hfile-records hfile))
+
+          (define token-records (filter-type record-type TOKEN records))]
+    (foldr (λ (f r) (cons (record-split f) r)) empty token-records)))
+
+; byrecord->byspine
+; SpineArity -> (listof (listof Natural))
+; produces the lolon of the spine arity, but numbers grouped by spine instead of record
+
+(define (byrecord->byspine spine-arity)
+  (local [(define number-global-spines (spine-arity-global spine-arity))
+
+          (define-struct acc (spine-list remaining))
+
+          ; (listof (listof Natural)) -> (listof (listof Natural))
+          (define (spine-iterator lolon)
+            ; num-spine: Natural. Number of spines processed so far.
+            ; spine-lists: (listof (listof Natural)). list of spine-list fields from byspine
+            ;
+            (local [(define (iterator num-spine spine-lists acc)
+                      (cond [(= num-spine number-global-spines) (reverse (cons (acc-spine-list acc) spine-lists))]
+                            [else
+                              (iterator (add1 num-spine)
+                                        (cons (acc-spine-list acc) spine-lists)
+                                        (byspine (acc-remaining acc)))]))]
+              (local [(define first-spine (byspine lolon))]
+                (iterator 1 empty (make-acc (acc-spine-list first-spine)
+                                            (acc-remaining  first-spine))))))
+
+          ; (listof (listof Natural)) -> acc
+          (define (byspine lolon)
+            ; spine-list: (listof Natural). list of number of tokens for a spine.
+            ; remaining: (listof (listof Natural)). list of remaining spine numbers.
+            ;
+            (local [(define (iterator arity spine-list remaining)
+                      (cond [(empty? arity) (make-acc (reverse spine-list) (reverse remaining))]
+                            [else
+                              (iterator (rest arity)
+                                        (cons (first (first arity)) spine-list)
+                                        (cons (rest (first arity)) remaining))]))]
+              (iterator lolon empty empty)))]
+    (spine-iterator (spine-arity-lolon spine-arity))))
