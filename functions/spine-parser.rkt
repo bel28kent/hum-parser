1d103e5e3a96144b1edd853641c06e39d30ae2c6
Bryan Bell <bjbsga@gmail.com>
2024-06-16 20:47:26 -0700

    op64 spines split by hum-parser

--- functions/spine-parser.rkt
+++ functions/spine-parser.rkt
@@ -12,19 +12,6 @@
 
 (define (spine-parser hfile spine-arity) (void))
 
-; 0. Unwrap tokens from records to create (listof (listof Token))
-; 1. For each global spine, make a lon from spine-arity for that spine:
-;        (spine-arity 2 (list (list 1 2)
-;                             (list 1 2))
-;    Becomes:
-;        (list (list 1 1)
-;              (list 2 2))
-; 2. a. For each new lon, take off the first number.
-;    b. Until a counter eq the first, copy first of lot off first record on to an accumulator
-;    c. Once counter eq the first, put the rest of lot on to an accumulator for the next spine
-;    d. Recurse with the rest of lon and rest of lolot
-; 3. With accumulator of 2b, make-global-spine for each lot
-
 ; unwrap
 ; HumdrumFile -> (listof (listof Token))
 ; produces the list of each record's list of tokens
@@ -135,9 +122,42 @@
                                    (add1 counter))]))]
     (lololot->logs lololot empty 0)))
 
+; logs->lolos
+; (listof GlobalSpine) -> (listof (listof String))
+; Converts each GlobalSpine to a (listof String)
 
+(define (logs->lolos logs)
+  (local [(define (logs-iterator logs)
+            ; lolos: (listof (listof String)). Accumulates los for each global spine.
+            ;
+            (local [(define (logs-iterator logs lolos)
+                      (cond [(empty? logs) (reverse lolos)]
+                            [else
+                              (logs-iterator (rest logs)
+                                             (cons (gs->los (first logs)) lolos))]))]
+              (logs-iterator logs empty)))
 
+          (define (gs->los gs)
+            (local [(define lolot (global-spine-tokens gs))
 
+                    (define (lolot-iterator lolot)
+                      (local [(define (lolot-iterator lolot los)
+                                ; los: (listof String). list of strings accumulated so far.
+                                ;
+                                (cond [(empty? lolot) (reverse los)]
+                                      [else
+                                        (lolot-iterator (rest lolot)
+                                                        (cons (lot->s (first lolot)) los))]))]
+                        (lolot-iterator lolot empty)))
+
+                    (define (lot->s lot)
+                      (cond [(empty? lot) ""]
+                            [else
+                              (if (empty? (rest lot))
+                                  (string-append (token-token (first lot)) (lot->s (rest lot)))
+                                  (string-append (token-token (first lot)) "\t" (lot->s (rest lot))))]))]
+              (lolot-iterator lolot)))]
+    (logs-iterator logs)))
 
 
 
