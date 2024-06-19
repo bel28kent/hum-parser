9cf8454c52330bd5e41cd6a7f30b455c99bb7ef2
Bryan Bell <bjbsga@gmail.com>
2024-06-19 09:16:27 -0700

    add header to tree definitions file

--- trees/data-definitions/data-definitions.rkt
+++ trees/data-definitions/data-definitions.rkt
@@ -1,1 +1,9 @@
 #lang racket
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; hum-parser: trees: data definitions
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(require "../../parser/data-definitions/data-definitions.rkt")
+
+(provide (all-defined-out))
