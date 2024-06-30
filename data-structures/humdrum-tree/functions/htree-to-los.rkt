c4b36e58447d37511dcdc68de45f2a16386b0d94
Bryan Bell <bjbsga@gmail.com>
2024-06-30 15:11:47 -0700

    adds htree-to-los file

---
+++ data-structures/humdrum-tree/functions/htree-to-los.rkt
@@ -0,0 +1,6 @@
+#lang racket
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; hum-parser: data-structures: HumdrumTree
+;;    htree->los: Converts HumdrumTree to (listof String)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
