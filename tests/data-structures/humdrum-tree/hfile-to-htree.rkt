8c0ba1c8c0a5468d1b908434cc3958e4e1e10299
Bryan Bell <bjbsga@gmail.com>
2024-07-01 13:07:31 -0700

    add test data for hfile to htree

--- tests/data-structures/humdrum-tree/hfile-to-htree.rkt
+++ tests/data-structures/humdrum-tree/hfile-to-htree.rkt
@@ -1,1 +1,14 @@
 #lang racket
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; hum-parser: tests for hfile->htree
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(require (only-in "../../../parser/functions/file.rkt" path->hfile)
+         (only-in "../../../data-structures/hfile-to-htree.rkt" hfile->htree)
+         test-engine/racket-tests)
+
+; hfile->htree
+()
+
+(test)
