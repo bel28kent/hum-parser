c17ffb2db4a7a78c4d19d921024f816e128419a3
Bryan Bell <bjbsga@gmail.com>
2024-08-01 14:40:14 -0700

    moves helper functions to top level for testing

--- data-structures/abstract-humdrum-graph/functions/hfile-to-ab-hgraph.rkt
+++ data-structures/abstract-humdrum-graph/functions/hfile-to-ab-hgraph.rkt
@@ -98,109 +98,108 @@
                                        (fn-for-lolot (rest lolot)
                                                      #f #f
                                                      spine-num))])))]
-              (fn-for-lolot lolot #f #f 1)))
-
-          ; TODO: time complexity
-          (define (get-token lot index caller)
-            (local [(define original lot)
-
-                    (define (get-token lot index counter)
-                      (cond [(empty? lot)
-                             (error
-                               "Reached an empty list before finding token."
-                               original
-                               index
-                               counter
-                               caller)]
-                            [else
-                              (if (= index counter)
-                                  (first lot)
-                                  (get-token (rest lot)
-                                             index
-                                             (add1 counter)))]))]
-              (get-token lot index 1)))
-
-          (define (splits-to-left? first-token-str lot spine-num)
-            (local [(define (splits-to-left? lot counter)
-                      (cond [(empty? lot) #f]
-                            [(= counter spine-num) #f]
-                            [else
-                              (if (spine-split? (token-token (first lot)))
-                                  #t
-                                  (splits-to-left? (rest lot)
-                                                   (add1 counter)))]))]
-              (if (or (spine-split? first-token-str)
-                      (spine-join? first-token-str)
-                      (null-interpretation? first-token-str))
-                  (splits-to-left? lot 1)
-                  #f)))
-
-          (define (trim-original original left right)
-            (local [(define left-last-index (token-record-number
-                                              (leaf-token
-                                                (first (reverse left)))))
-
-                    (define right-last-index (token-record-number
-                                               (leaf-token
-                                                 (first (reverse right)))))
-
-                    (define record-index (if (< left-last-index
-                                                right-last-index)
-                                             left-last-index
-                                             right-last-index))
-
-                    (define (trim-original original)
-                      (cond [(empty? original)
-                             (error
-                               "Reached empty list before finding index.")]
-                            [(= record-index (token-record-number
-                                               (first (first original))))
-                             (handle-join (rest original) left right)]
-                            [else
-                              (trim-original (rest original))]))]
-              (trim-original original)))
-
-          (define (handle-join rest-original left right)
-            (if (string=? "*v" (token-token (first (first rest-original))))
-                (if (join-is-already-paired? (first (first rest-original))
-                                             left
-                                             right)
-                    (rest rest-original)
-                    rest-original)
-                rest-original))
-
-          ; produces true if join can be found twice between left and right
-          (define (join-is-already-paired? join left right)
-            (local [(define (counter lot)
-                      (cond [(empty? lot) 0]
-                            [else
-                              (if (equal? join (first lot))
-                                  (add1 (counter (rest lot)))
-                                  (counter (rest lot)))]))
-
-                    (define left-count (counter (branch->lot left)))
-
-                    (define right-count (counter (branch->lot right)))]
-              (if (= 2 (+ left-count right-count))
-                  #t
-                  #f)))
-
-          ; collapses a branch of a tree to a (listof Token)
-          (define (branch->lot branch)
-            (local [(define (branch->lot branch acc)
-                      (cond [(empty? branch) (reverse acc)]
-                            [(leaf? (first branch))
-                             (branch->lot (rest branch)
-                                          (cons (leaf-token (first branch))
-                                                acc))]
-                            [else
-                              (branch->lot (append (parent-left (first branch))
-                                                   (parent-right (first branch))
-                                                   (rest branch))
-                                           (cons (parent-token (first branch))
-                                                 acc))]))]
-              (branch->lot branch empty)))]
+              (fn-for-lolot lolot #f #f 1)))]
     (type (root (fn-for-logs spines)))))
+
+; TODO: time complexity
+(define (get-token lot index caller)
+  (local [(define original lot)
+
+          (define (get-token lot index counter)
+            (cond [(empty? lot) (error
+                                  "Reached an empty list before finding token."
+                                  original
+                                  index
+                                  counter
+                                  caller)]
+                  [else
+                    (if (= index counter)
+                        (first lot)
+                        (get-token (rest lot)
+                                   index
+                                   (add1 counter)))]))]
+    (get-token lot index 1)))
+
+(define (splits-to-left? first-token-str lot spine-num)
+  (local [(define (splits-to-left? lot counter)
+            (cond [(empty? lot) #f]
+                  [(= counter spine-num) #f]
+                  [else
+                    (if (spine-split? (token-token (first lot)))
+                        #t
+                        (splits-to-left? (rest lot)
+                                         (add1 counter)))]))]
+    (if (or (spine-split? first-token-str)
+            (spine-join? first-token-str)
+            (null-interpretation? first-token-str))
+        (splits-to-left? lot 1)
+        #f)))
+
+(define (trim-original original left right)
+  (local [(define left-last-index (token-record-number
+                                    (leaf-token
+                                      (first (reverse left)))))
+
+          (define right-last-index (token-record-number
+                                     (leaf-token
+                                       (first (reverse right)))))
+
+          (define record-index (if (< left-last-index
+                                      right-last-index)
+                                   left-last-index
+                                   right-last-index))
+
+          (define (trim-original original)
+            (cond [(empty? original)
+                   (error
+                     "Reached empty list before finding index.")]
+                  [(= record-index (token-record-number
+                                     (first (first original))))
+                   (handle-join (rest original) left right)]
+                  [else
+                    (trim-original (rest original))]))]
+    (trim-original original)))
+
+(define (handle-join rest-original left right)
+  (if (string=? "*v" (token-token (first (first rest-original))))
+      (if (join-is-already-paired? (first (first rest-original))
+                                   left
+                                   right)
+          (rest rest-original)
+          rest-original)
+      rest-original))
+
+; produces true if join can be found twice between left and right
+(define (join-is-already-paired? join left right)
+  (local [(define (counter lot)
+            (cond [(empty? lot) 0]
+                  [else
+                    (if (equal? join (first lot))
+                        (add1 (counter (rest lot)))
+                        (counter (rest lot)))]))
+
+          (define left-count (counter (branch->lot left)))
+
+          (define right-count (counter (branch->lot right)))]
+    (if (= 2 (+ left-count right-count))
+        #t
+        #f)))
+
+; collapses a branch of a tree to a (listof Token)
+(define (branch->lot branch)
+  (local [(define (branch->lot branch acc)
+            (cond [(empty? branch) (reverse acc)]
+                  [(leaf? (first branch))
+                   (branch->lot (rest branch)
+                                (cons (leaf-token (first branch))
+                                      acc))]
+                  [else
+                   (branch->lot (append (parent-left (first branch))
+                                        (parent-right (first branch))
+                                        (rest branch))
+                                (cons (parent-token (first branch))
+                                      acc))]))]
+    (branch->lot branch empty)))
 
 (define one-spine-no-splits    "../../../tests/data-structures/data/one-spine-no-splits.krn")
 (define one-spine-one-split    "../../../tests/data-structures/data/one-spine-one-split.krn")
