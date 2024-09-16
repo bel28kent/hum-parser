d92b899003be89fcbb964408a584df63b6ca1e11
Bryan Bell <bjbsga@gmail.com>
2024-09-16 14:20:51 -0700

    add empty check-expect forms

--- tests/count-and-order-tests/tests/order/spine-splits-left-joins-left.rkt
+++ tests/count-and-order-tests/tests/order/spine-splits-left-joins-left.rkt
@@ -2,6 +2,13 @@
 
 (require test-engine/racket-tests)
 
+(check-expect (path->hfile ) )
+(check-expect (spine-parser ) )
+(check-expect (ab-hgraph->hfile ) )
+(check-expect (lolot->lor ) )
+(check-expect (ab-hgraph->lolot ) )
+(check-expect (hfile->ab-hgraph ) )
+(check-expect (branch->lot ) )
 **kern
 *^
 *^	*
