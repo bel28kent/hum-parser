f81a6f2fb2be181e54aab832c015cdb32ec45bf2
Bryan Bell <bjbsga@gmail.com>
2024-10-30 16:42:19 -0700

    remove Abstract from data definitions file

--- data-structures/humdrum-graph/data-definitions/data-definitions.rkt
+++ data-structures/humdrum-graph/data-definitions/data-definitions.rkt
@@ -1,7 +1,7 @@
 #lang racket/base
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; hum-parser: data structures: AbstractHumdrumGraph
+;; hum-parser: data structures: HumdrumGraph
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (require racket/list
@@ -10,19 +10,16 @@
 (provide (all-defined-out))
 
 #|
-    An AbstractHumdrumGraph is a graph. It is the supertype of
-    HumdrumTree and HumdrumGraph. These data structures are identical
-    in their implementations, but differ in how they may be traversed.
-    The AbstractHumdrumGraph and its subtypes are best imagined as
-    trees with branches and nodes. They are two-dimensional and allow
-    for vertical (depth-first) and horizontal (breadth-first) traversal.
-    In a musical context, these are akin to melodic and harmonic
-    analysis.
+    A HumdrumGraph is a graph. A HumdrumGraph is best imagined as
+    trees with branches and nodes, though it allows for joins.
+    They are two-dimensional and afford vertical (depth-first)
+    and horizontal (breadth-first) traversal. In a musical context,
+    these are akin to melodic and harmonic analysis.
 
-    An AbstractHumdrumGraph has an arbitrary number of branches.
-    Each branch represents one global spine in a Humdrum file.
-    Branches are ordered; the leftmost branch is the leftmost spine
-    of the Humdrum file.
+    A HumdrumGraph has an arbitrary number of branches. Each branch
+    represents one global spine in a Humdrum file. Branches are
+    ordered; the leftmost branch is the leftmost spine of the Humdrum
+    file.
 
     A branch is a list of nodes. This list is also ordered, with the
     first node of each branch being the exclusive interpretation
@@ -34,8 +31,7 @@
     right sub-branch.
 |#
 
-(struct abstract-humdrum-graph (root) #:constructor-name ab-hgraph
-                                      #:transparent)
+(struct humdrum-graph (root) #:constructor-name hgraph #:transparent)
 
 (struct root (branches) #:transparent)
 ; Root is (root (listof (listof Node)))
@@ -60,49 +56,36 @@
 ;  Represents a node with two children. The last
 ;  token in each list is a spine-join.
 
-;; Examples
-;;  For examples of more complicated data, see
-;;  tests/data-structures/abstract-humdrum-graph/hfile-to-ab-hgraph.rkt
+; Empty HumdrumGraph
+(define EMPTY-HGRAPH (hgraph (root empty)))
 
-; Empty AbstractHumdrumGraph
-(define EMPTY-AB-HGRAPH (ab-hgraph (root empty)))
+; HumdrumGraph with one spine, no splits
+(define SIMPLE-AB-HGRAPH (hgraph (root (list (list
+                                              (leaf (token "**kern" EXCLUSIVE-INTERPRETATION 0))
+                                              (leaf (token "4a" SPINE-DATA 1))
+                                              (leaf (token "4b" SPINE-DATA 2)))))))
 
-; AbstractHumdrumGraph with one spine, no splits
-(define SIMPLE-AB-HGRAPH (ab-hgraph
-                           (root
-                             (list
-                               (list
-                                 (leaf
-                                   (token "**kern" EXCLUSIVE-INTERPRETATION 0))
-                                 (leaf (token "4a" SPINE-DATA 1))
-                                 (leaf (token "4b" SPINE-DATA 2)))))))
+; HumdrumGraph with two spines, no splits
+(define HGRAPH-TWO-SPINES (hgraph (root (list (list
+                                               (leaf (token "**kern" EXCLUSIVE-INTERPRETATION 0))
+                                               (leaf (token "4a" SPINE-DATA 1))
+                                               (leaf (token "4b" SPINE-DATA 2)))
+                                              (list
+                                               (leaf (token "**dynam" EXCLUSIVE-INTERPRETATION 0))
+                                               (leaf (token "f" #f 1))
+                                               (leaf (token "p" #f 2)))))))
 
-; AbstractHumdrumGraph with two spines, no splits
-(define AB-HGRAPH-TWO-SPINES
-        (ab-hgraph
-          (root
-            (list
-              (list (leaf (token "**kern" EXCLUSIVE-INTERPRETATION 0))
-                    (leaf (token "4a" SPINE-DATA 1))
-                    (leaf (token "4b" SPINE-DATA 2)))
-              (list (leaf (token "**dynam" EXCLUSIVE-INTERPRETATION 0))
-                    (leaf (token "f" #f 1))
-                    (leaf (token "p" #f 2)))))))
-
-; AbstractHumdrumGraph with one spine, splits
-(define AB-HGRAPH-ONE-SPLITS
-        (ab-hgraph
-          (root
-            (list
-              (list (leaf (token "**kern" EXCLUSIVE-INTERPRETATION 0))
-                    (leaf (token "4a" SPINE-DATA 1))
-                    (parent (token "*^" SPINE-SPLIT 2)
-                            (list (leaf (token "4a" SPINE-DATA 3))
-                                  (leaf (token "4a" SPINE-DATA 4))
-                                  (leaf (token "*v" SPINE-JOIN 5)))
-                            (list (leaf (token "4aa" SPINE-DATA 3))
-                                  (leaf (token "4aa" SPINE-DATA 4))
-                                  (leaf (token "*v" SPINE-JOIN 5))))
-                    (leaf (token "4a" SPINE-DATA 6))
-                    (leaf (token "4b" SPINE-DATA 7))
-                    (leaf (token "4c" SPINE-DATA 8)))))))
+; HumdrumGraph with one spine, splits
+(define HGRAPH-ONE-SPLITS (hgraph (root (list (list
+                                               (leaf (token "**kern" EXCLUSIVE-INTERPRETATION 0))
+                                               (leaf (token "4a" SPINE-DATA 1))
+                                               (parent (token "*^" SPINE-SPLIT 2)
+                                                       (list (leaf (token "4a" SPINE-DATA 3))
+                                                             (leaf (token "4a" SPINE-DATA 4))
+                                                             (leaf (token "*v" SPINE-JOIN 5)))
+                                                       (list (leaf (token "4aa" SPINE-DATA 3))
+                                                             (leaf (token "4aa" SPINE-DATA 4))
+                                                             (leaf (token "*v" SPINE-JOIN 5))))
+                                               (leaf (token "4a" SPINE-DATA 6))
+                                               (leaf (token "4b" SPINE-DATA 7))
+                                               (leaf (token "4c" SPINE-DATA 8)))))))
