0c02ccda3a9b1ed25da1f377b688ac10a77db1c2
Bryan Bell <bjbsga@gmail.com>
2024-10-31 12:49:41 -0700

    update humdrum-graph templates

--- parser/data-structures/humdrum-graph/functions/templates.rkt
+++ parser/data-structures/humdrum-graph/functions/templates.rkt
@@ -9,11 +9,54 @@
 
 ;; Melodic or Depth-first traversal
 ;; (Regular recursion)
-;;    This traversal depends on whether the data structure is
-;;    a HumdrumTree or HumdrumGraph. See:
-;;    ../../humdrum-tree/functions/templates.rkt
-;;    OR
-;;    ../../humdrum-graph/functions/templates.rkt
+;;    This template starts at the root of the graph, and
+;;    pursues the leftmost branch as far down as it can
+;;    before moving to the adjacent, right-hand branch.
+;;    This procedure is repeated through the traversal
+;;    of the rightmost branch.
+;;
+;;    This traversal is akin to a melodic analysis, going
+;;    through each part note-by-note, with no awareness of
+;;    simultaneous events.
+
+(define (fn-for-hgraph hgraph)
+  (local [(define (fn-for-root root)
+            (local [(define (iterator branches)
+                      (cond [(empty? branches) ...]
+                            [else
+                              (... (fn-for-lon (first branches))
+                                   (iterator (rest branches)))]))]
+              (iterator (root-branches root))))
+
+          (define (fn-for-lon branch)
+            (cond [(empty? branch) ...]
+                  [else
+                    ; if (parent? first), left and right sub-branch
+                    ; will be combined in fn-for-parent, and then
+                    ; combined here with the rest
+                    (... (fn-for-node (first branch))
+                         (fn-for-lon (rest branch)))]))
+
+          (define (fn-for-node node)
+            (cond [(leaf? node) (fn-for-leaf node)]
+                  [else
+                    (fn-for-parent node)]))
+
+          (define (fn-for-leaf leaf)
+            (... (fn-for-token (leaf-token leaf))))
+
+          (define (fn-for-parent parent)
+            ; combination of recursive calls on left and right sub-branches
+            (... (fn-for-token (parent-token parent))
+                 (fn-for-lon (parent-left parent))
+                 (fn-for-lon (parent-right parent))))
+
+          (define (fn-for-token token)
+            (... (token-token token)
+                 (token-type token)
+                 (token-record-number token)
+                 (token-field-index token)))]
+    (... (fn-for-root (humdrum-graph-root hgraph)))))
 
 ;; Harmonic or Breadth-first traversal
 ;; (Recursion with accumulators)
@@ -34,7 +77,7 @@
 ;;    when the (empty? branches) is reached, passing that second
 ;;    accumulator to the analysis function.
 
-(define (fn-for-ab-hgraph ab-hgraph)
+(define (fn-for-hgraph hgraph)
   (local [(define (fn-for-root root)
                     ; acc. (listof (listof Node)). the rest of each branch.
             (local [(define (iterator branches acc)
@@ -63,5 +106,6 @@
           (define (fn-for-token token)
             (... (token-token token)
                  (token-type token)
-                 (token-record-number token)))]
-    (... (fn-for-root (abstract-humdrum-graph-root ab-hgraph)))))
+                 (token-record-number token)
+                 (token-field-index token)))]
+    (... (fn-for-root (humdrum-graph-root hgraph)))))
