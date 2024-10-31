1ea2b6bb6dc89a8cc828a1e0f63a95a4393e0318
Bryan Bell <bjbsga@gmail.com>
2024-10-31 14:17:19 -0700

    update paths in longest-string-in tests

--- tests/parser/data-structures/humdrum-graph/longest-string-in.rkt
+++ tests/parser/data-structures/humdrum-graph/longest-string-in.rkt
@@ -6,16 +6,14 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (require
-  (only-in "../../../parser/functions/file.rkt"
+  (only-in "../../../../parser/functions/file.rkt"
            path->hfile)
-  "../../../data-structures/abstract-humdrum-graph/data-definitions/data-definitions.rkt"
-  "../../../data-structures/abstract-humdrum-graph/functions/hfile-to-ab-hgraph.rkt"
-  "../../../data-structures/abstract-humdrum-graph/functions/longest-string-in.rkt"
+  "../../../../parser/data-structures/humdrum-graph/functions/hfile-to-hgraph.rkt"
+  "../../../../parser/data-structures/humdrum-graph/functions/longest-string-in.rkt"
   test-engine/racket-tests)
 
 (define three-spines-path "../data/three-spines-two-splits-not-consecutive.krn")
-(define three-spines-no-splits (hfile->ab-hgraph (path->hfile three-spines-path)
-                                                 ab-hgraph))
+(define three-spines-no-splits (hfile->hgraph (path->hfile three-spines-path)))
 
 ; longest-string-in
 (check-expect (longest-string-in three-spines-no-splits) "2.C#] 2.B]")
