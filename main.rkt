8e9d7bc47032382a470194387d5ea54cfb004696
Bryan Bell <bjbsga@gmail.com>
2024-11-03 14:55:14 -0800

    update paths in main

--- main.rkt
+++ main.rkt
@@ -19,25 +19,16 @@
             (all-from-out "parser/functions/type.rkt")))
 
 (module data-structures racket/base
-  (require  "data-structures/abstract-humdrum-graph/data-definitions/data-definitions.rkt"
-            (only-in "data-structures/abstract-humdrum-graph/functions/ab-hgraph-to-hfile.rkt"
-                     ab-hgraph->hfile)
-            "data-structures/abstract-humdrum-graph/functions/hfile-to-ab-hgraph.rkt"
-            "data-structures/abstract-humdrum-graph/functions/longest-string-in.rkt"
-            "data-structures/humdrum-tree/data-definitions/data-definitions.rkt"
-            (only-in "data-structures/humdrum-tree/functions/visualize-htree.rkt"
-                     visualize-htree)
-            "data-structures/humdrum-graph/data-definitions/data-definitions.rkt"
-            (only-in "data-structures/humdrum-graph/functions/visualize-hgraph.rkt"
-                     visualize-hgraph))
-  (provide  (all-from-out "data-structures/abstract-humdrum-graph/data-definitions/data-definitions.rkt")
-            ab-hgraph->hfile
-            (all-from-out "data-structures/abstract-humdrum-graph/functions/hfile-to-ab-hgraph.rkt")
-            (all-from-out "data-structures/abstract-humdrum-graph/functions/longest-string-in.rkt")
-            (all-from-out "data-structures/humdrum-tree/data-definitions/data-definitions.rkt")
-            (all-from-out "data-structures/humdrum-graph/data-definitions/data-definitions.rkt")
-            visualize-htree
-            visualize-hgraph))
+  (require  "parser/data-structures/humdrum-graph/data-definitions/data-definitions.rkt"
+            (only-in "parser/data-structures/humdrum-graph/functions/hgraph-to-hfile.rkt"
+                     hgraph->hfile)
+            (only-in "parser/data-structures/humdrum-graph/functions/hfile-to-hgraph.rkt"
+                     hfile->hgraph)
+            "parser/data-structures/humdrum-graph/functions/longest-string-in.rkt")
+  (provide  (all-from-out "parser/data-structures/humdrum-graph/data-definitions/data-definitions.rkt")
+            hgraph->hfile
+            hfile->hgraph
+            (all-from-out "parser/data-structures/humdrum-graph/functions/longest-string-in.rkt")))
 
 (require 'parser
          'data-structures)
