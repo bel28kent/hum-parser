9d7fcd5e844fd719780c1899e20a97a78e6b2798
Bryan Bell <bjbsga@gmail.com>
2025-04-07 19:26:08 -0700

    exclusive predicates

--- parser/data-definitions/ExclusiveInterpretation.rkt
+++ parser/data-definitions/ExclusiveInterpretation.rkt
@@ -6,6 +6,33 @@
 	Implemented as (hash Symbol RegularExpression ...)
 |#
 
+(require racket/contract
+         racket/list
+         racket/local)
+
+(provide ExclusiveInterpretation
+         exclusive-interpretation?
+         exclusive-interpretation-match?)
+
 (define ExclusiveInterpretation (hash 'Dynam "^\\*{2}dynam"
                                       'Kern  "^\\*{2}kern"
+                                      'Neume "^\\*{2}neume"
+                                      'Text  "^\\*{2}text"
 ))
+
+; TODO: abstract here and in TandemInterpretation
+(define/contract (exclusive-interpretation? str)
+  (-> string? boolean?)
+  (local [(define (exclusive-interpretation? keys)
+            (cond [(empty? keys) #f]
+                  [(exclusive-interpretation-match? (first keys) str) #t]
+                  [else
+                    (exclusive-interpretation? (rest keys))]))]
+    (exclusive-interpretation? (hash-keys ExclusiveInterpretation))))
+
+; TODO: abstract here and in TandemInterpretation
+(define/contract (exclusive-interpretation-match? interp str)
+  (-> symbol? string? boolean?)
+  (regexp-match? (pregexp
+                   (hash-ref ExclusiveInterpretation interp))
+                 str))
