661b18c8fabd789f38929f4d0c05abdf2bbc041d
Bryan Bell <bjbsga@gmail.com>
2025-08-27 18:11:26 -0400

    notes

--- parser/func-abstract.rkt
+++ parser/func-abstract.rkt
@@ -10,12 +10,14 @@
          racket/local)
 
 (provide filter-type
-         hash-match?
-         hash-member?
-         get-type)
+         member?)
 
 (define/contract (filter-type type-accessor symbol los)
   (-> struct-accessor-procedure? symbol? (listof struct?) (listof struct?))
   (local [(define (is-type? s)
             (symbol=? (type-accessor s) symbol))]
     (filter is-type? los)))
+
+(define (member? l e)
+  ; TODO: returns #t if element e is a member of list l, else #f
+  #f)
--- parser/func-hgraph.rkt
+++ parser/func-hgraph.rkt
@@ -1,29 +1,53 @@
 #lang racket/base
 
 #|
-	HumdrumGraph Functions.
+	HumdrumGraph functions.
 |#
 
-(require racket/list)
+(require racket/list
+         racket/local
+         "data-HumdrumGraph.rkt"
+         "func-abstract.rkt")
 
 ;; Basic operations
 ;; NB: A HumdrumGraph is immutable, so add, remove, and set are invalid.
 
-(define (adjacent? graph vertex_1 vertex_2)
-  ; TODO: returns true if there is an edge from vertex_1 to vertex_2
-  #f)
+(define (adjacent? edges vertex_1 vertex_2)
+  (if (member? (hash-ref edges vertex_1) vertex_2)
+       #t
+       #f))
 
-(define (children graph vertex)
-  ; TODO: returns a list of vertices with an edge to vertex
-  empty)
+(define (children edges vertex)
+  (hash-ref edges vertex))
 
-(define (siblings graph vertex)
+; TODO: optional argument to only return result of right-siblings, useful for bft.
+(define (siblings hgraph vertex)
   ; TODO: returns a list of vertices that are siblings to vertex
   ; cf. NB in data-HumdrumGraph.rkt. This will involve asking
   ; if the next element(s) in the list that this vertex came from
   ; are siblings (spine split case) and looking for siblings in
   ; the neighboring spines.
-  empty)
+  (local [(define (spine-split ...)
+            ; TODO: returns a list of vertices that are siblings
+            ; to vertex AND in the same spine as vertex (spine
+            ; split case.
+            ; returns list of length >=0
+            empty)
+
+          (define (left-siblings ...)
+            ; TODO: returns a list of vertices that are siblings
+            ; to vertex AND in any spines to the left of this
+            ; vertex's spine.
+            ; returns list of length >=0
+            empty)
+
+          (define (right-siblings ...)
+            ; TODO: returns a list of vertices that are siblings
+            ; to vertex AND in any spines to the right of this
+            ; vertex's spine.
+            ; returns list of length >=0
+            empty)]
+    ()))
 
 ;; Converters
 
@@ -37,12 +61,16 @@
 
 ;; Traversal
 
-(define (depth-first-traversal graph)
+(define (depth-first-traversal hgraph)
   ; TODO: traverses the graph depth first and does ...
   ;       returns ...
   (...))
 
-(define (breadth-first-traversal graph)
+(define (breadth-first-traversal hgraph)
   ; TODO: traverses the graph breadth first and does ...
   ;       returns ...
   (...))
+
+; (depth-first-search ...)
+
+; (breadth-first-seach ...)
